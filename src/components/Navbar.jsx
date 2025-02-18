a607720e6c1690460b218b429e163e09c02d46f8
ShivaniMhatre <shivanikatkar97@gmail.com>
2025-02-18 19:51:32 +0530

    Single Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ export default function Navbar() {
 
         {/* logo */}
         <div className='nav__logo'>
-          <Link to="/">Ecom<span>.</span></Link>
+          <Link to="/">Shivani<span>.</span></Link>
         </div>
 
         {/* nav icons */}
